9b2249a9810c1d22ef16604a893864a0ffc86b89
Maxim Navodnikov <0>
2014-12-18 18:51:04 +0000

    List implementation

--- GameLogics.hs
+++ GameLogics.hs
@@ -1,6 +1,4 @@
-import Data.Array
-import Data.Array.ST
-import Data.Array.MArray
+module GameLogics where
 
 horizontalCellsCount = 10
 verticalCellsCount = 10
@@ -10,14 +8,19 @@ middleV = verticalCellsCount `div` 2
 
 matrixIndex row_i col_i col_size = row_i * col_size + col_i
 
-middleCell = matrixIndex middleH middleV verticalCellsCount
+fieldIndex row_i col_i = matrixIndex row_i col_i verticalCellsCount
+
+middleCell = fieldIndex middleH middleV
 
 snake = [middleCell, middleCell - 1, middleCell - 2]
 
 apple = undefined
 
-field snake_l = elems $ runSTArray $ do
-  let list0 = (take (horizontalCellsCount * verticalCellsCount) $ repeat 0)
-  arr <- newListArray (0, length list0 - 1) list0
-  writeArray arr middleCell 1
-  return arr
+field snake_l = [paintSnakeCell cell_v | cell_v <- gen_l]
+  where
+    gen_l = [0 .. (horizontalCellsCount * verticalCellsCount) - 1]
+    paintSnakeCell cell_v
+      | cell_v == head snake_l = 1
+      | elem cell_v snake_l = 2
+      | otherwise = 0
+
